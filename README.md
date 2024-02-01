910e2701b3738fb48cec19450d7d6d54680172f0
Lennart Klein <31488915+kleinlennart@users.noreply.github.com>
2024-02-02 00:18:06 +0100

    add research paper template

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # My .devcontainer Templates
 
-## blog-template
+## [blog-template](blog-template/)
 
 - -> what I use for my blog kleinbutsignificant.com
 - R
@@ -13,6 +13,11 @@
 - mainly intended for GitHub Codespaces
 - RStudio Server
 
-## R-Python
+## [R-Python](R-Python/)
 
 - showcase how to include Python
+
+## [research-paper](research-paper/) 🚧
+
+- basic template for an R project used for reproduciable research
+
---
+++ research-paper/postCreateCommand.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+# postCreateCommand.sh
+
+## restore packages as defined in the lockfile
+R -e 'renv::restore()'
+
+# renv::snapshot(type = "explicit", dev = TRUE)
+
+# TODO: fix X11 bug
+# sudo apt-get update -y && sudo apt-get install -y libxt6
+
+# https://github.com/rocker-org/devcontainer-features/tree/main/src/quarto-cli#execution-engine
+# python3 -m pip install --upgrade pip
+
+## Essential for quarto
+# python3 -m pip install jupyter
+# python3 -m pip install jupyter-cache
+# quarto check jupyter
+
+## Install all other env packages
+# python3 -m pip install -r ./.devcontainer/requirements.txt
+# python3 -m pip freeze > ./.devcontainer/requirements.txt
+
+# https://docs.github.com/en/codespaces/setting-up-your-project-for-codespaces/adding-a-dev-container-configuration/setting-up-your-python-project-for-codespaces
+# // Use 'postCreateCommand' to run commands after the container is created.
+# "postCreateCommand": "pip3 install --user -r requirements.txt",
+
+# "postCreateCommand": "echo 'options(repos = c(CRAN = \"https://cloud.r-project.org\"))' | sudo sh -c 'cat - >>\"${R_HOME}/etc/Rprofile.site\"'",
